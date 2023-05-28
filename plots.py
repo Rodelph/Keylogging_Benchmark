4147085b8f8b29ffa47f33140685c1c731d50a94
Peter <1822913@stud.hs-mannheim.de>
2023-05-28 21:01:03 +0200

    jo mama

--- plots.py
+++ plots.py
@@ -3,16 +3,16 @@
 from os import listdir
 from matplotlib import pyplot as plt
 import numpy as np
-path="/home/swt/Programme/ossec/benchmarks"
+path="/home/swt/Programme/Keylogging_Benchmark/benchmarks/"
 
 files = [f for f in listdir(path) if "txt" in str(f)]
 files = sorted(files)
-encr = {32:[], 64:[], 128:[], 256:[], 512:[], 1024:[], 2048:[], 4096:[]}
-decr = {32:[], 64:[], 128:[], 256:[], 512:[], 1024:[], 2048:[], 4096:[]}
-mem = {32:[], 64:[], 128:[], 256:[], 512:[], 1024:[], 2048:[], 4096:[]}
+encr = {32:[], 128:[], 512:[], 2048:[]}
+decr = {32:[], 128:[], 512:[], 2048:[]}
+mem = {32:[], 128:[], 512:[], 2048:[]}
 
 for file in files:
-    f = open(file, "r")
+    f = open(path+file, "r")
     for line in f.readlines():
         if "encr" in line:
             encr[int(file.split(".")[0])].append(float(line.split()[-2]))
@@ -31,7 +31,7 @@ for i in range(len(axs)):
     blowfish = encr[list(encr.keys())[i]][3:]
     axs[i, 0].bar(np.arange(5)-0.1, aes, width=0.2, color="orange", label="AES")
     axs[i, 0].bar(np.arange(5)+0.1, blowfish, width=0.2, color="blue", label="Blowfish")
-    axs[i, 0].set_ylabel(str(list(encr.keys())[i]) + "\nTime in ms")
+    axs[i, 0].set_ylabel(str(list(encr.keys())[i]) + "\n\n\nTime in ms")
     axs[i, 0].set_xticklabels(["0", "32", "64", "128", "192", "256"])
 
     aes = [0, 0] + decr[list(decr.keys())[i]][0:3]
@@ -53,4 +53,5 @@ axs[0, 2].set_title("Memory usage")
 axs[3, 0].set_xlabel("Key size")
 axs[3, 1].set_xlabel("Key size")
 axs[3, 2].set_xlabel("Key size")
+axs[0, 0].legend(loc='upper left')
 plt.show()
